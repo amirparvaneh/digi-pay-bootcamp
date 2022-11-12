bc3fc2eab9dafa701d8527e6495b4daf0f7743e0
amirparvaneh <amirparvaneh138@gmail.com>
2022-11-13 00:06:23 +0330

    adding category

--- src/main/java/com/digipay/inventory/controller/CategoryController.java
+++ src/main/java/com/digipay/inventory/controller/CategoryController.java
@@ -4,9 +4,12 @@ package com.digipay.inventory.controller;
 import com.digipay.inventory.model.category.Category;
 import com.digipay.inventory.service.categoryService.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/category")
 public class CategoryController {
@@ -22,4 +25,11 @@ public class CategoryController {
     private void addNewCategory(@RequestBody Category category){
         categoryService.save(category);
     }
+
+    @GetMapping()
+    private List<Category> getAllCategories(){
+        return categoryService.findAll();
+    }
+
+
 }
